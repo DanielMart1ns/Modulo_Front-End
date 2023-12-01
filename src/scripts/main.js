4749490f1b0ed48409d0ba5b955f79b7088d3a96
Daniel <134549131+DanielMart1ns@users.noreply.github.com>
2023-12-01 15:44:07 -0300

    Add files via upload

---
+++ src/scripts/main.js
@@ -0,0 +1,54 @@
+var img1 = document.getElementsByTagName('img')[0]
+var img2 = document.getElementsByTagName('img')[1]
+var img3 = document.getElementsByTagName('img')[2]
+var img4 = document.getElementsByTagName('img')[3]
+var img5 = document.getElementsByTagName('img')[4]
+var img6 = document.getElementsByTagName('img')[5]
+
+img1.addEventListener('mouseenter', function(){
+    img1.style.boxShadow = '0px 0px 10px #00000091'
+
+    img1.addEventListener('mouseout', function(){
+        img1.style.boxShadow = ''
+    })
+})
+
+img2.addEventListener('mouseenter', function(){
+    img2.style.boxShadow = '0px 0px 10px #00000091'
+
+    img2.addEventListener('mouseout', function(){
+        img2.style.boxShadow = ''
+    })
+})
+
+img3.addEventListener('mouseenter', function(){
+    img3.style.boxShadow = '0px 0px 10px #00000091'
+
+    img3.addEventListener('mouseout', function(){
+        img3.style.boxShadow = ''
+    })
+})
+
+img4.addEventListener('mouseenter', function(){
+    img4.style.boxShadow = '0px 0px 10px #00000091'
+
+    img4.addEventListener('mouseout', function(){
+        img4.style.boxShadow = ''
+    })
+})
+
+img5.addEventListener('mouseenter', function(){
+    img5.style.boxShadow = '0px 0px 10px #00000091'
+
+    img5.addEventListener('mouseout', function(){
+        img5.style.boxShadow = ''
+    })
+})
+
+img6.addEventListener('mouseenter', function(){
+    img6.style.boxShadow = '0px 0px 10px #00000091'
+
+    img6.addEventListener('mouseout', function(){
+        img6.style.boxShadow = ''
+    })
+})
