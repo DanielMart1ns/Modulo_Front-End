7aa10d081c8353f63f7908224d541fd4385f5935
DanielMart1ns <danielmmrodrigues09@gmail.com>
2024-01-26 17:16:02 -0300

    feat: inclusão de campo de busca

--- scripts/main.js
+++ scripts/main.js
@@ -1,27 +1,42 @@
 document.addEventListener('DOMContentLoaded', ()=>{
-    const avatar = document.querySelector('#avatar')
-    const name = document.querySelector('#name')
-    const userName = document.querySelector('#userName')
+    const form = document.querySelector('#formulario')
 
-    const repositories = document.querySelector('#repos')
-    const followers = document.querySelector('#followers')
-    const following = document.querySelector('#following')
+    form.addEventListener('submit', (evento)=>{
+        evento.preventDefault();
 
-    const userLink = document.querySelector('#userLink')
+        const dados = document.querySelector('#dadosDePesquisa').value
 
-    const http = 'https://api.github.com/users/DanielMart1ns'
+        const avatar = document.querySelector('#avatar')
+        const name = document.querySelector('#name')
+        const userName = document.querySelector('#userName')
 
-    fetch(http)
-        .then(function(resposta){
-            return resposta.json()
-        })
-        .then(function(json){
-            avatar.src = json.avatar_url
-            name.innerHTML = json.name
-            userName.innerHTML = json.login
-            repositories.innerText = json.public_repos
-            followers.innerText = json.followers
-            following.innerText = json.following
-            userLink.href = json.html_url
-        })
+        const repositories = document.querySelector('#repos')
+        const followers = document.querySelector('#followers')
+        const following = document.querySelector('#following')
+
+        const userLink = document.querySelector('#userLink')
+
+        const http = `https://api.github.com/users/${dados}`
+
+        fetch(http)
+            .then(function(resposta){
+                return resposta.json()
+            })
+            .then(function(json){
+                    avatar.src = json.avatar_url
+                    name.innerHTML = json.name
+                    userName.innerHTML = json.login
+                    repositories.innerText = json.public_repos
+                    followers.innerText = json.followers
+                    following.innerText = json.following
+                    userLink.href = json.html_url
+
+                    document.querySelector('.resultado').style.display = 'block'
+            })
+            .catch(function(error){
+                alert(`Página não encontrada ${error}`)
+                document.querySelector('body').classList.add('Error')
+                document.querySelector('header').classList.add('Error')
+            })
+    })
 })
